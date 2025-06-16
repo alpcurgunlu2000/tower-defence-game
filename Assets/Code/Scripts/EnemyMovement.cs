3e19367a3cefdf44353f9112a63eade524f980aa
JoshivP <joshiv18122001@gmail.com>
2025-06-16 17:36:05 +0200

    Erster Code vom Tutorial (Mit Compile Fehler)

---
+++ Assets/Code/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb; // This will allow us to move our enemy
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target; // Point we want to move to
+    private int pathIndex = 0;
+
+    private void Start()
+    {
+        target = LevelManager.main.path[pathIndex];
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
+            pathIndex++;
+            if (pathIndex == LevelManager.main.path.Length)
+            {
+                EnemySpawner.onEnemyDestroy.Invoke(); // It will call the function from EnemySpawner
+                Destroy(gameObject); // Wenn Enemy reaches the End of the Path
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.path[pathIndex];
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * moveSpeed;
+    }
+}
+
+
+
---
+++ Assets/Code/Scripts/EnemySpawner.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPreFabs;
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;   // 8 Enemies will be spawned
+    [SerializeField] private float enemiesPerSecond = 0.5f;   // 8 Enemies will be spawned
+    [SerializeField] private float timeBetweenWaves = 5f;   // 5 Seconds between the Waves
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
+
+    [Header("Events")]
+    public static UnityEvent onEnemyDestroy = new UnityEvent();
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
+
+    private void Start()
+    {
+        Debug.Log("EnemySpawner Start called.");
+        StartCoroutine(StartWave());
+
+    }
+
+    private void Awake()
+    {
+        onEnemyDestroy.AddListener(EnemyDestroyed);
+    }
+
+    private void Update()
+    {
+        if (!isSpawning)
+        {
+            Debug.Log("Not spawning yet...");
+            return;
+        }
+        Debug.Log("Spawning is active. Enemies left: " + enemiesLeftToSpawn);
+        timeSinceLastSpawn += Time.deltaTime; // It makes the Rounds harder by Spawning faster every Round
+         if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
+
+        {
+            SpawnEnemy();
+            enemiesLeftToSpawn--;
+            enemiesAlive++;
+            timeSinceLastSpawn = 0f;
+        }
+
+        if (enemiesAlive == 0 && enemiesLeftToSpawn == 0)
+        {
+            EndWave();
+        }
+    }
+
+    
+    private void EnemyDestroyed()
+    {
+        enemiesAlive--;
+
+    }
+
+    private void SpawnEnemy()
+    {
+        Debug.Log("Spawn Enemy");
+        GameObject prefabToSpawn = enemyPreFabs[0];
+        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+    }
+
+    private IEnumerator StartWave()
+    {
+        yield return new WaitForSeconds(timeBetweenWaves);
+        isSpawning = true;
+        enemiesLeftToSpawn = EnemiesPerWave();
+    }
+
+    private void EndWave()
+    {
+        isSpawning = false;
+        timeSinceLastSpawn = 0f;
+        StartCoroutine(StartWave());
+    }
+
+    private int EnemiesPerWave()
+    {
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
+}
